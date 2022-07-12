d4af02a5a7420bcc267c4309ea7b3cdf1030a0dd
Junwei <junwei.he.99@gmail.com>
2022-07-11 17:23:25 -0700

    Secured API key

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL ='http://www.omdbapi.com?apikey=18999d2b';
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`${process.env.API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
